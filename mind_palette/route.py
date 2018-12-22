b6bf0f2da8f96d84b033a4109a350d88a8f933c0
kimdoori <dudu@spoqa.com>
2018-12-23 00:35:26 +0900

    기본 layout 구성

--- mind_palette/route.py
+++ mind_palette/route.py
@@ -5,3 +5,28 @@ from flask import render_template
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/major")
+def major():
+    return render_template("major.html")
+
+
+@app.route("/job")
+def job():
+    return render_template("job.html")
+
+
+@app.route("/university")
+def university():
+    return render_template("university.html")
+
+
+@app.route("/certificate")
+def certificate():
+    return render_template("certificate.html")
+
+
+@app.route("/competition")
+def competition():
+    return render_template("competition.html")
